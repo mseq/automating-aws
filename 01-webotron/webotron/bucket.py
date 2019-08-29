8db3fcd3a4fbc392ee505fdad9f8c12305962f31
Marcelo Sequeiros <mseq@globo.com>
2019-08-29 17:25:43 -0300

    Configure DNS with Route53

--- 01-webotron/webotron/bucket.py
+++ 01-webotron/webotron/bucket.py
@@ -16,7 +16,7 @@ import util
 class BucketManager:
     """Manage an S3 Bucket."""
 
-    CHUNK_SIZE = 8 * 1024 * 1024 # 8mb.
+    CHUNK_SIZE = 8388608
 
     def __init__(self, session):
         """Create a BucketManager object."""
@@ -27,6 +27,10 @@ class BucketManager:
             multipart_chunksize=self.CHUNK_SIZE,
         )
         self.manifest = {}
+
+    def get_bucket(self, bucket_name):
+        """Get a bucket by name."""
+        return self.s3.Bucket(bucket_name)
 
     def get_region_name(self, bucket):
         """Get the bucket's region name."""
@@ -53,12 +57,15 @@ class BucketManager:
         """Create new bucket, or return existing one by name."""
         s3_bucket = None
         try:
-            s3_bucket = self.s3.create_bucket(
-                Bucket=bucket_name,
-                CreateBucketConfiguration={
-                    'LocationConstraint': self.session.region_name
-                }
-            )
+            if self.session.region_name == 'us-east-1':
+                s3_bucket = self.s3.create_bucket(Bucket=bucket_name)
+            else:
+                s3_bucket = self.s3.create_bucket(
+                    Bucket=bucket_name,
+                    CreateBucketConfiguration={
+                        'LocationConstraint': self.session.region_name
+                    }
+                )
         except ClientError as error:
             if error.response['Error']['Code'] == 'BucketAlreadyOwnedByYou':
                 s3_bucket = self.s3.Bucket(bucket_name)
@@ -101,7 +108,7 @@ class BucketManager:
         })
 
     def load_manifest(self, bucket):
-        """Load manifest for caching purposes.""" 
+        """Load manifest for caching purposes."""
         paginator = self.s3.meta.client.get_paginator('list_objects_v2')
         for page in paginator.paginate(Bucket=bucket.name):
             for obj in page.get('Contents', []):
@@ -133,7 +140,8 @@ class BucketManager:
             elif len(hashes) == 1:
                 return '"{}"'.format(hashes[0].hexdigest())
             else:
-                hash = self.hash_data(reduce(lambda x, y: x + y, (h.digest() for h in hashes)))
+                hash = self.hash_data(reduce(lambda x, y: x + y,
+                                      (h.digest() for h in hashes)))
                 return '"{}-{}"'.format(hash.hexdigest(), len(hashes))
 
     def upload_file(self, bucket, path, key):
--- 01-webotron/webotron/webotron.py
+++ 01-webotron/webotron/webotron.py
@@ -17,17 +17,20 @@ import boto3
 import click
 
 from bucket import BucketManager
+from domain import DomainManager
+import util
 
 
 session = None
 bucket_manager = None
+domain_manager = None
 
 
 @click.group()
 @click.option('--profile', default=None, help="Use a given AWS profile")
 def cli(profile):
     """Webtron deploys websites to AWS."""
-    global session, bucket_manager
+    global session, bucket_manager, domain_manager
 
     session_cfg = {}
     if profile:
@@ -35,6 +38,7 @@ def cli(profile):
 
     session = boto3.Session(**session_cfg)
     bucket_manager = BucketManager(session)
+    domain_manager = DomainManager(session)
 
 
 @cli.command('list-buckets')
@@ -69,6 +73,20 @@ def sync(pathname, bucket):
     # bucket_manager.s3.Bucket(bucket)
     bucket_manager.sync(pathname, bucket)
     print(bucket_manager.get_bucket_url(bucket_manager.s3.Bucket(bucket)))
+
+
+@cli.command('setup-domain')
+@click.argument('domain')
+def setup_domain(domain):
+    """Condigure a DOMAIN to point to BUCKET."""
+    bucket = bucket_manager.get_bucket(domain)
+    zone = domain_manager.find_hosted_zone(domain) \
+        or domain_manager.create_hosted_zone(domain)
+
+    endpoint = util.get_endpoint(bucket_manager.get_region_name(bucket))
+    domain_manager.create_s3_domain_record(zone, domain, endpoint)
+
+    print("Domain configure: http://{}".format(domain))
 
 
 if __name__ == '__main__':
